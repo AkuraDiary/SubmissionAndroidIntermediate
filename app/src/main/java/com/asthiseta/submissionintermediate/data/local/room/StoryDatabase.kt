4d9b3634a649e48325ae9590640d8c345387aa90
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-08-17 18:28:20 +0700

    added room and databases classess

---
+++ app/src/main/java/com/asthiseta/submissionintermediate/data/local/room/StoryDatabase.kt
@@ -0,0 +1,13 @@
+package com.asthiseta.submissionintermediate.data.local.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.asthiseta.submissionintermediate.data.local.entity.RemoteKeyEntity
+import com.asthiseta.submissionintermediate.data.model.stories.Story
+
+
+@Database(entities = [Story::class, RemoteKeyEntity::class], version = 1, exportSchema = false)
+abstract class StoryDatabase : RoomDatabase() {
+    abstract fun storyDao(): StoryDao
+    abstract fun remoteKeyDao(): RemoteKeyDao
+}
