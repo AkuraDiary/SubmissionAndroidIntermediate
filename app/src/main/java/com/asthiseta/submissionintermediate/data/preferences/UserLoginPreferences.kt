7ee3e411b4a21806ea855072644946b065bda168
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-08-17 19:57:13 +0700

    edited user preferences to implement dagger

--- app/src/main/java/com/asthiseta/submissionintermediate/data/preferences/UserLoginPreferences.kt
+++ app/src/main/java/com/asthiseta/submissionintermediate/data/preferences/UserLoginPreferences.kt
@@ -2,49 +2,60 @@ package com.asthiseta.submissionintermediate.data.preferences
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.asthiseta.submissionintermediate.BuildConfig.PREF_NAME
 import com.asthiseta.submissionintermediate.data.model.auth.UsrSession
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
-class UserLoginPreferences(context: Context) {
+val  Context.datastore : DataStore<Preferences> by preferencesDataStore(name = PREF_NAME)
+
+class UserLoginPreferences @Inject constructor(@ApplicationContext val context: Context) {
+    private val dataStore = context.datastore
+
     private val pref: SharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
     private val editor = pref.edit()
 
-    fun setUsrLogin(user: UsrSession){
-        editor.apply{
-            putString(NAME_KEY, user.name)
-            putString(TOKEN_KEY, user.token)
-            putString(USER_ID_KEY, user.userId)
-            putBoolean(STATE_KEY, user.isLogin)
-            apply()
+    suspend fun setUsrLogin(user: UsrSession){
+        dataStore.edit {
+            it[NAME_KEY] = user.name
+            it[TOKEN_KEY] = user.token
+            it[USER_ID_KEY] = user.userId
+            it[STATE_KEY]  = false
         }
     }
 
-    fun logout(){
-        editor.apply {
-            remove(NAME_KEY)
-            remove(TOKEN_KEY)
-            remove(USER_ID_KEY)
-            putBoolean(STATE_KEY, false)
-            apply()
+
+    suspend  fun logout(){
+        dataStore.edit {
+            it[NAME_KEY] = ""
+            it[TOKEN_KEY] = ""
+            it[USER_ID_KEY] = ""
+            it[STATE_KEY]  = false
         }
     }
 
-    fun getLoginData(): UsrSession {
-
-        return UsrSession(
-            pref.getString(NAME_KEY, "").toString(),
-            pref.getString(TOKEN_KEY, "").toString(),
-            pref.getString(USER_ID_KEY, "").toString(),
-            pref.getBoolean(STATE_KEY, false)
-        )
-    }
-
+    fun getLoginData(): Flow<UsrSession> {
+        return dataStore.data.map {
+            UsrSession(
+            it[TOKEN_KEY].toString(),
+            it[NAME_KEY].toString(),
+            it[USER_ID_KEY].toString(),
+            it[STATE_KEY] ?: false
+            )
+        }}
     companion object{
-        private const val NAME_KEY = "NAME"
-        private const val TOKEN_KEY = "TOKEN"
-        private const val USER_ID_KEY = "USER_ID"
-        private const val STATE_KEY = "STATE"
-
+        private  val NAME_KEY = stringPreferencesKey("NAME")
+        private  val TOKEN_KEY = stringPreferencesKey("TOKEN")
+        private  val USER_ID_KEY = stringPreferencesKey("USER_ID")
+        private  val STATE_KEY = booleanPreferencesKey("STATE")
 
     }
 }
