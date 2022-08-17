5c4de0cba4b414262684b5beb0357815ccb9f3ab
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-08-18 01:03:44 +0700

    added DataStoreVM

---
+++ app/src/main/java/com/asthiseta/submissionintermediate/data/preferences/DataStoreVM.kt
@@ -0,0 +1,30 @@
+package com.asthiseta.submissionintermediate.data.preferences
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.asthiseta.submissionintermediate.data.model.auth.UsrSession
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DataStoreVM @Inject constructor(private val pref: UserLoginPreferences) : ViewModel(){
+
+    fun getLoginSession(): LiveData<UsrSession>{
+        return pref.getLoginData().asLiveData()
+    }
+
+    fun setLoginSession(loggedUser: UsrSession){
+        viewModelScope.launch {
+            pref.setUsrLogin(loggedUser)
+        }
+    }
+
+    fun logout(){
+        viewModelScope.launch {
+            pref.logout()
+        }
+    }
+}
