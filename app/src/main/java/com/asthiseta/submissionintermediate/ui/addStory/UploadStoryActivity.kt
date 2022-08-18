2f642378eb3bd764b73a71fb8b86883f824cc3ca
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-08-18 10:17:28 +0700

    edited upload story mechanism

--- app/src/main/java/com/asthiseta/submissionintermediate/ui/addStory/UploadStoryActivity.kt
+++ app/src/main/java/com/asthiseta/submissionintermediate/ui/addStory/UploadStoryActivity.kt
@@ -8,13 +8,15 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+
 import com.asthiseta.submissionintermediate.ui.activities.MainActivity
 import com.asthiseta.submissionintermediate.data.model.stories.AddStoryResponse
+import com.asthiseta.submissionintermediate.data.preferences.DataStoreVM
 import com.asthiseta.submissionintermediate.data.preferences.UserLoginPreferences
-import com.asthiseta.submissionintermediate.data.remote.RetrofitConfig
 import com.asthiseta.submissionintermediate.databinding.ActivityUploadStoryBinding
 import com.asthiseta.submissionintermediate.utilities.UploadStoryUtilities
 import com.asthiseta.submissionintermediate.utilities.UploadStoryUtilities.reduceFileImage
@@ -31,17 +33,15 @@ import java.io.File
 
 class UploadStoryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityUploadStoryBinding
-
     private lateinit var currentPath: String
-    private lateinit var usrLoginPref: UserLoginPreferences
-
     private var getFile: File? = null
+    private val dataStoreVM by viewModels<DataStoreVM>()
+    private val uploadVM by viewModels<UploadVM>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUploadStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        usrLoginPref = UserLoginPreferences(this)
         supportActionBar?.title = "Upload Story"
         initView()
 
@@ -147,52 +147,22 @@ class UploadStoryActivity : AppCompatActivity() {
         if (getFile != null) {
             val file = reduceFileImage(getFile as File)
             val descriptionText = binding.editTextAddDescription.text.toString()
-            val description = descriptionText.toRequestBody("text/plain".toMediaType())
-            val requestImageFile = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
-            val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
-                "photo",
-                file.name,
-                requestImageFile
-            )
-
-            val token = "Bearer ${usrLoginPref.getLoginData().token}"
-            val service = RetrofitConfig.getApiService().uploadStory(token, imageMultipart, description)
-            service.enqueue(object : Callback<AddStoryResponse> {
-                override fun onResponse(call: Call<AddStoryResponse>, response: Response<AddStoryResponse>) {
-                    if (response.isSuccessful) {
-                        val responseBody = response.body()
-                        if (responseBody != null && !responseBody.error) {
-                            FancyToast.makeText(
-                                this@UploadStoryActivity,
-                                responseBody.message,
-                                FancyToast.LENGTH_SHORT,
-                                FancyToast.SUCCESS,
-                                false
-                            ).show()
-                            onBackPressed()
-                        } else {
-                            FancyToast.makeText(
-                                this@UploadStoryActivity,
-                                response.message(),
-                                FancyToast.LENGTH_SHORT,
-                                FancyToast.ERROR,
-                                false
-                            ).show()
-                        }
+            dataStoreVM.getLoginSession().observe(this){
+                uploadVM.apply {
+                    uploadStory("Bearer ${it.token}", file, descriptionText)
+                    message.observe(this@UploadStoryActivity){
+                        FancyToast.makeText(
+                            this@UploadStoryActivity,
+                            it,
+                            FancyToast.LENGTH_SHORT,
+                            FancyToast.INFO,
+                            false
+                        ).show()
                     }
                 }
 
-                override fun onFailure(call: Call<AddStoryResponse>, t: Throwable) {
-                    FancyToast.makeText(
-                        this@UploadStoryActivity,
-                        t.message.toString(),
-                        FancyToast.LENGTH_SHORT,
-                        FancyToast.ERROR,
-                        false
-                    ).show()
-                }
+            }
 
-            })
         } else {
             FancyToast.makeText(
                 this@UploadStoryActivity,
---
+++ app/src/main/java/com/asthiseta/submissionintermediate/ui/addStory/UploadVM.kt
@@ -0,0 +1,17 @@
+package com.asthiseta.submissionintermediate.ui.addStory
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.asthiseta.submissionintermediate.data.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import retrofit2.http.Multipart
+import java.io.File
+import javax.inject.Inject
+
+@HiltViewModel
+class UploadVM @Inject constructor(private val repos : Repository): ViewModel() {
+    val message : LiveData<String> = repos.message
+    fun uploadStory(token: String, image : File, desc:String){
+        repos.uploadStory(token, image, desc)
+    }
+}
