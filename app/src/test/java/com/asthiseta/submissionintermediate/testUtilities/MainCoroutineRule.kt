42522aa5c5668928378f94764ffd28260bb4256c
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-08-20 23:16:56 +0700

    alhamdulillah akhirnya

--- app/src/test/java/com/asthiseta/submissionintermediate/testUtilities/MainCoroutineRule.kt
+++ app/src/test/java/com/asthiseta/submissionintermediate/testUtilities/MainCoroutineRule.kt
@@ -1,21 +1,22 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 package com.asthiseta.submissionintermediate.testUtilities
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestDispatcher
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.test.*
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
+
 @ExperimentalCoroutinesApi
-class MainDispatcherRule(
-    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
-) : TestWatcher() {
+class MainCoroutineRule(
+    val dispatcher: TestDispatcher = StandardTestDispatcher()) :
+    TestWatcher(){
     override fun starting(description: Description) {
-        Dispatchers.setMain(testDispatcher)
+        Dispatchers.setMain(dispatcher)
     }
+
     override fun finished(description: Description) {
         Dispatchers.resetMain()
     }
