81c6ce27d8f1f73be63a31a79d941c74ab79244f
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-08-22 13:48:34 +0700

    added test for auth viewmodel

---
+++ app/src/test/java/com/asthiseta/submissionintermediate/ui/maps/MapsVMTest.kt
@@ -0,0 +1,16 @@
+package com.asthiseta.submissionintermediate.ui.maps
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class MapsVMTest {
+
+    @Test
+    fun getStoryList() {
+    }
+
+    @Test
+    fun getStoriesWithLocations() {
+    }
+}
